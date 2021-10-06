a08ea74cd96496f79b8d9a92bb703a5538bd2658
Leandro Arbelo <leo.arbelo22@gmail.com>
2021-10-06 20:20:44 -0300

    frontend base realizado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PresentacionComponent } from './presentacion/presentacion.component';
+import { EstudiosComponent } from './estudios/estudios.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { ConocimientosComponent } from './conocimientos/conocimientos.component';
+import { OtrosComponent } from './otros/otros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PresentacionComponent,
+    EstudiosComponent,
+    ExperienciaComponent,
+    ConocimientosComponent,
+    OtrosComponent
   ],
   imports: [
     BrowserModule
